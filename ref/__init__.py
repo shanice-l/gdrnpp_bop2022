033de61041107895f652bdb5d49aab528cf48979
liuxingyu <lxy17@foxmail.com>
2022-10-15 20:36:36 +0800

    upd ref

--- ref/__init__.py
+++ ref/__init__.py
@@ -1,1 +1,1 @@
-from . import lm_full, lmo_full, ycbv, hb, hb_bop19, hb_bdp, sphere_synt, lm_duck_fabi, tudl, tless, icbin, itodd, nocs
+from . import lmo_full, ycbv, hb, hb_bop19, tudl, tless, icbin, itodd
