e137941ce24ae0f0566abd64e4e16eb2cdcce1a8
Peter Hoenig <hpfreak1004@gmail.com>
2022-12-02 11:46:36 +0100

    updated docker run command

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# replace file paths of the three volumes with the paths to your /datasets, /pretrained_models and /output folders
+# prepare /datasets, /pretrained_models and /output folders as explained in the main README.md
+gdrnpp_dir=${PWD%/*}
 
 docker run \
 --gpus all \
 -it \
 --shm-size=8gb --env="DISPLAY" \
---volume="/home/hoenig/BOP/datasets:/gdrnpp_bop2022/datasets" \
---volume="/home/hoenig/BOP/gdrnets/gdrnpp_bop2022/pretrained_models:/gdrnpp_bop2022/pretrained_models" \
---volume="/home/hoenig/BOP/gdrnets/gdrnpp_bop2022/output:/gdrnpp_bop2022/output" \
+--volume="${gdrnpp_dir}/datasets:/gdrnpp_bop2022/datasets" \
+--volume="${gdrnpp_dir}/pretrained_models:/gdrnpp_bop2022/pretrained_models" \
+--volume="${gdrnpp_dir}/output:/gdrnpp_bop2022/output" \
 --name=gdrnppv0 gdrnpp
