5d0f5e34d841a0c4400d19c080ed7ecfd66eff07
Peter Hoenig <hpfreak1004@gmail.com>
2023-01-23 13:22:34 +0100

    dont clone repo in the container, instead mount it with a volume for easier debugging

--- docker/README.md
+++ docker/README.md
@@ -1,4 +1,5 @@
 # Dockerfile for GDRNPP
-Configure volume-paths in docker_run.sh before usage \
-```bash docker_build.sh```
-```bash docker_run.sh```
+Configure volume-paths in docker_run.sh before usage  
+`bash docker_build.sh`  
+`bash docker_run.sh`  
+`sh scripts/compile_all.sh`
--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -7,6 +7,7 @@ docker run \
 --gpus all \
 -it \
 --shm-size=8gb --env="DISPLAY" \
+--volume="${gdrnpp_dir}:/gdrnpp_bop2022" \
 --volume="${gdrnpp_dir}/datasets:/gdrnpp_bop2022/datasets" \
 --volume="${gdrnpp_dir}/pretrained_models:/gdrnpp_bop2022/pretrained_models" \
 --volume="${gdrnpp_dir}/output:/gdrnpp_bop2022/output" \
